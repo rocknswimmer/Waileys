829a491e63375cdaea743b472de9455e5d0ddb91
Andy Simmons <rocknswimmer116@gmail.com>
2026-04-30 15:05:39 -0400

    ready for app properties

---
+++ server/src/test/java/waileys/data/KnownGoodState.java
@@ -0,0 +1,28 @@
+package waileys.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+
+@Component
+public class KnownGoodState {
+    @Autowired
+    private static JdbcTemplate jdbcTemplate;
+
+    private boolean hasRun = false;
+
+    public static void set() {
+        jdbcTemplate.execute((Connection conn) -> {
+            try (CallableStatement cs = conn.prepareCall("call set_known_good_state()")) {
+                return cs.execute();
+            }
+        });
+    }
+
+    public void reset() {
+        hasRun = false;
+    }
+}
--- server/src/test/java/waileys/data/RunnerJdbcTemplateRepositoryTest.java
+++ server/src/test/java/waileys/data/RunnerJdbcTemplateRepositoryTest.java
@@ -1,7 +1,12 @@
 package waileys.data;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import waileys.models.Runner;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,6 +15,20 @@ class RunnerJdbcTemplateRepositoryTest {
     @Autowired
     RunnerJdbcTemplateRepository repository;
 
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup(){
+        KnownGoodState.set();
+    }
+
+    @Test
+    void shouldFindAll(){
+        List<Runner> runners = repository.findAll();
+        assertNotNull(runners);
+        assertTrue(runners.size() > 0);
+    }
 
 
 }
