92e78e988178cf899ddc68a45fe38c3612c4fad2
Andy Simmons <rocknswimmer116@gmail.com>
2026-04-30 14:50:30 -0400

    test script created

---
+++ server/sql/test.sql
@@ -0,0 +1,30 @@
+drop database if exists waileys_test;
+create database waileys_test;
+use waileys_test;
+
+CREATE TABLE runners (
+  runner_id int primary key auto_increment,
+  runner VARCHAR(100) NOT NULL,
+  pace REAL NOT NULL
+);
+
+CREATE TABLE legs (
+  leg_id int primary key auto_increment,
+  runner_id int not null,
+  distance REAL NOT NULL,
+  complete BOOLEAN NOT NULL DEFAULT false,
+  start_time VARCHAR(50) NOT NULL DEFAULT 'TBD',
+  end_time VARCHAR(50) NOT NULL DEFAULT 'TBD',
+  mountain_start VARCHAR(50) NOT NULL DEFAULT 'TBD',
+  mountain_end VARCHAR(50) NOT NULL DEFAULT 'TBD',
+  pacific_start VARCHAR(50) NOT NULL DEFAULT 'TBD',
+  pacific_end VARCHAR(50) NOT NULL DEFAULT 'TBD',
+  dif VARCHAR(50) NOT NULL DEFAULT 'TBD',
+  constraint fk_leg_runner foreign key(runner_id)
+  references runners(runner_id)
+);
+
+insert into runners (runner, pace) values ('And e', 10);
+
+
+insert into legs (runner_id, distance) values (1, 3.1);
